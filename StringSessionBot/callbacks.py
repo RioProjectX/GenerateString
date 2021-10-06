48a865fbc8d920d33202f123a0069a2cfeb19db2
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-06 22:38:38 +0700

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -43,7 +43,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         )
     elif query == "generate":
         await callback_query.message.reply(
-            "Please choose the python library you want to generate string session for",
+            "Silahkan Pilih String Mana Yang Ingin Kamu Ambil 😁",
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
                 InlineKeyboardButton("Telethon", callback_data="telethon")
