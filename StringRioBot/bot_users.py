d362f945ca560a0ba9e221bc581bf194e89a54a8
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-06-14 16:13:57 +0700

    v2.3 update clear by rio

---
+++ StringRioBot/bot_users.py
@@ -0,0 +1,21 @@
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from StringGenBot.db import SESSION
+from StringGenBot.db.users_sql import Users, num_users
+
+
+@Client.on_message(~filters.service, group=1)
+async def users_sql(_, msg: Message):
+    if msg.from_user:
+        q = SESSION.query(Users).get(int(msg.from_user.id))
+        if not q:
+            SESSION.add(Users(msg.from_user.id))
+            SESSION.commit()
+        else:
+            SESSION.close()
+
+
+@Client.on_message(filters.user(1356469075) & filters.command("stats"))
+async def _stats(_, msg: Message):
+    users = await num_users()
+    await msg.reply(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ String Rio Bot :\n\n {users} ᴜsᴇʀs", quote=True)
